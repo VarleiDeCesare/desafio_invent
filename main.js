d9e704433e66c6d6b1f432ff784fa3a35311e570
VarleiDeCesare <varleidecesare@yahoo.com.br>
2022-01-11 13:42:54 -0300

    Resolução do problema Proposto. * Escreva uma função que recebe um parâmetro string referente a um nome completo de uma pessoa (tamanho variável) e reduz para no máximo 13 caracteres. * Exemplos:
    	* Input: Yan Rui Barbosa
    	* Output: Yan Rui B.
    
    	* Input: Yan Da Silva Fonseca
    	* Output: Yan Da S. F.

---
+++ main.js
@@ -0,0 +1,19 @@
+
+function main(name) {
+    const nomeSeparado = name.split(" ")
+    let novoNome = '';
+    contador = 0;
+
+    for (let i = 0; i < nomeSeparado.length; i++) {
+        contador += nomeSeparado[i].length + 1;
+        if (contador >= 13) {
+            novoNome += nomeSeparado[i][0] + ". ";
+            continue
+        }
+        novoNome += nomeSeparado[i] + " ";
+    }
+    console.log(novoNome);
+}
+
+
+main("Yan Da Silva Fonseca")
